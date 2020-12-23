8c65572902f89cf352888320ea949c6ff22c53cd
Tianbai Xiao <tianbaixiao@gmail.com>
2020-12-23 10:53:26 +0100

    Update test_io.jl

--- test/test_io.jl
+++ test/test_io.jl
@@ -1,3 +1,8 @@
 cd(@__DIR__)
 allowed = ["case", "space", "flux", "collision"]
 D = KitBase.read_dict("config.txt", allowed)
+
+ks, ctr, face, simTime = KitBase.initialize("config.txt")
+KitBase.write_jld(ks, ctr)
+
+KitBase.plot_line(ks, ctr; backend = :plots::Symbol)
