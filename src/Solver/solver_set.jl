80d6445e8f8d0ff04ec1c0ce4ed61fe19209fdc4
Tianbai Xiao <tianbaixiao@gmail.com>
2020-12-31 14:01:49 +0100

    Update SolverSet

--- src/Solver/solver_set.jl
+++ src/Solver/solver_set.jl
@@ -2,239 +2,275 @@
 Structure of solver setup
 
 """
-struct SolverSet <: AbstractSolverSet
-
+struct SolverSet{TS<:AbstractSetup,TP<:AbstractPhysicalSpace,TV<:AbstractVelocitySpace,TG<:AbstractProperty,TI<:AbstractCondition,TO<:AbstractString} <: AbstractSolverSet
     # setup
-    set::AbstractSetup
+    set::TS
+    # physical space
+    pSpace::TP
+    # velocity space
+    vSpace::TV
+    # gas property
+    gas::TG
+    # initial and boundary condition
+    ib::TI
+    # file system
+    outputFolder::TO
+end
+
+function SolverSet(configfilename::T) where {T<:AbstractString}
+    # generate variables from configuration file
+    dict = read_dict(configfilename)
+    for key in keys(dict)
+        s = Symbol(key)
+        @eval $s = $(dict[key])
+    end
+
+    # set
+    set = Setup(
+        case,
+        space,
+        flux,
+        collision,
+        nSpecies,
+        interpOrder,
+        limiter,
+        boundary,
+        cfl,
+        maxTime,
+    )
 
     # physical space
-    pSpace::AbstractPhysicalSpace
+    pSpace = set_geometry(dict)
 
     # velocity space
-    vSpace::AbstractVelocitySpace
+    vSpace = set_velocity(dict)
 
     # gas property
-    gas::AbstractProperty
+    gas = set_property(dict)
 
-    # initial and boundary condition
-    ib::AbstractCondition
+    if @isdefined uLid
+        ib = set_ib(set, vSpace, gas, uLid, vLid, tLid)
+    else
+        ib = set_ib(set, vSpace, gas)
+    end
 
-    # file system
-    outputFolder::String
+    # create working directory
+    identifier = string(Dates.now(), "/")
+    #outputFolder = string("../out/", replace(identifier, ":"=>"."))
+    outputFolder = replace(identifier, ":" => ".")
+    mkdir(outputFolder)
+    mkdir(string(outputFolder, "data/"))
+    cp(configfilename, string(outputFolder, "config.txt"))
 
-    # constructor
-    SolverSet(set, pSpace, vSpace, gas, ib, outputFolder) =
-        new(set, pSpace, vSpace, gas, ib, outputFolder)
+    # create new struct
+    return SolverSet{typeof(set),typeof(pSpace),typeof(vSpace),typeof(gas),typeof(ib),typeof(outputFolder)}(set, pSpace, vSpace, gas, ib, outputFolder)
+end
 
-    function SolverSet(configfilename::T) where {T<:AbstractString}
 
-        # generate variables from configuration file
-        dict = read_dict(configfilename)
-        for key in keys(dict)
-            s = Symbol(key)
-            @eval $s = $(dict[key])
-        end
+"""
+Generate AbstractPhysicalSpace
 
-        # set
-        set = Setup(
-            case,
-            space,
-            flux,
-            collision,
-            nSpecies,
-            interpOrder,
-            limiter,
-            boundary,
-            cfl,
-            maxTime,
-        )
+"""
+function set_geometry(dict::T) where {T<:AbstractDict}
+    for key in keys(dict)
+        s = Symbol(key)
+        @eval $s = $(dict[key])
+    end
 
-        # physical space
-        Dx = parse(Int, space[1])
-        if Dx == 1
-            pSpace = PSpace1D(x0, x1, nx, nxg)
-        elseif Dx == 2
-            pSpace = PSpace2D(x0, x1, nx, y0, y1, ny, nxg, nyg)
-        else
-            throw("No preset available for 3D simulation, please set it up manually.")
-        end
+    Dx = parse(Int, space[1])
+    if Dx == 1
+        pSpace = PSpace1D(x0, x1, nx, nxg)
+    elseif Dx == 2
+        pSpace = PSpace2D(x0, x1, nx, y0, y1, ny, nxg, nyg)
+    else
+        throw("No preset available for 3D simulation, please set it up manually.")
+    end
 
-        # velocity space
-        Dv = parse(Int, space[5])
-        if Dv == 1
-            if nSpecies == 1
-                vSpace = VSpace1D(umin, umax, nu, vMeshType, nug)
-            elseif nSpecies == 2
-                ue0 = umin * sqrt(mi / me)
-                ue1 = umax * sqrt(mi / me)
-                vSpace = MVSpace1D(umin, umax, ue0, ue1, nu, vMeshType, nug)
-            else
-                throw("The velocity space only supports up to two species.")
-            end
-        elseif Dv == 2
-            if nSpecies == 1
-                vSpace = VSpace2D(umin, umax, nu, vmin, vmax, nv, vMeshType, nug, nvg)
-            elseif nSpecies == 2
-                ue0 = umin * sqrt(mi / me)
-                ue1 = umax * sqrt(mi / me)
-                ve0 = vmin * sqrt(mi / me)
-                ve1 = vmax * sqrt(mi / me)
-                vSpace = MVSpace2D(
-                    umin,
-                    umax,
-                    ue0,
-                    ue1,
-                    nu,
-                    vmin,
-                    vmax,
-                    ve0,
-                    ve1,
-                    nv,
-                    vMeshType,
-                    nug,
-                    nvg,
-                )
-            else
-                throw("The velocity space only supports up to two species.")
-            end
-        elseif Dv == 3
-            if nSpecies == 1
-                vSpace = VSpace3D(
-                    umin,
-                    umax,
-                    nu,
-                    vmin,
-                    vmax,
-                    nv,
-                    wmin,
-                    wmax,
-                    nw,
-                    vMeshType,
-                    nug,
-                    nvg,
-                    nwg,
-                )
-            elseif nSpecies == 2
-                ue0 = umin * sqrt(mi / me)
-                ue1 = umax * sqrt(mi / me)
-                ve0 = vmin * sqrt(mi / me)
-                ve1 = vmax * sqrt(mi / me)
-                we0 = wmin * sqrt(mi / me)
-                we1 = wmax * sqrt(mi / me)
-                vSpace = MVSpace3D(
-                    umin,
-                    umax,
-                    ue0,
-                    ue1,
-                    nu,
-                    vmin,
-                    vmax,
-                    ve0,
-                    ve1,
-                    nv,
-                    wmin,
-                    wmax,
-                    we0,
-                    we1,
-                    nw,
-                    vMeshType,
-                    nug,
-                    nvg,
-                    nwg,
-                )
-            else
-                throw("The velocity space only supports up to two species.")
-            end
-        end
+    return pSpace
+end
 
-        # gas property
-        γD = map(parse(Int, space[3]), parse(Int, space[5])) do x, y # (x)f(y)v
-            if x == 1 # 1f
-                if y >= 3 # 3v
-                    return 3
-                else # 1v
-                    return Dx
-                end
-            elseif x == 2 # 2f
-                return Dx
-            elseif x >= 3 # 3f / 4f
-                return 3
-            else
-                return nothing
-            end
-        end
-        γ = heat_capacity_ratio(inK, γD)
 
+"""
+Generate AbstractVelocitySpace
+
+"""
+function set_velocity(dict::T) where {T<:AbstractDict}
+    for key in keys(dict)
+        s = Symbol(key)
+        @eval $s = $(dict[key])
+    end
+
+    Dv = parse(Int, space[5])
+    if Dv == 1
         if nSpecies == 1
-            μᵣ = ref_vhs_vis(knudsen, alphaRef, omegaRef)
-            gas = Gas(knudsen, mach, prandtl, inK, γ, omega, alphaRef, omegaRef, μᵣ)
+            vSpace = VSpace1D(umin, umax, nu, vMeshType, nug)
         elseif nSpecies == 2
-            kne = knudsen * (me / mi)
-            if !(@isdefined rL) && !(@isdefined echi) # undefined plasma args
-                gas = Mixture([knudsen, kne], mach, prandtl, inK, γ, mi, ni, me, ne)
-            else
-                if Dx == 1
-                    gas = Plasma1D(
-                        [knudsen, kne],
-                        mach,
-                        prandtl,
-                        inK,
-                        γ,
-                        mi,
-                        ni,
-                        me,
-                        ne,
-                        lD,
-                        rL,
-                        sol,
-                        echi,
-                        bnu,
-                    )
-                elseif Dx == 2
-                    gas = Plasma2D(
-                        [knudsen, kne],
-                        mach,
-                        prandtl,
-                        inK,
-                        γ,
-                        mi,
-                        ni,
-                        me,
-                        ne,
-                        lD,
-                        rL,
-                        sol,
-                        echi,
-                        bnu,
-                    )
-                else
-                    throw("The plasma property only supports up to 2D case.")
-                end
+            ue0 = umin * sqrt(mi / me)
+            ue1 = umax * sqrt(mi / me)
+            vSpace = MVSpace1D(umin, umax, ue0, ue1, nu, vMeshType, nug)
+        else
+            throw("The velocity space only supports up to two species.")
+        end
+    elseif Dv == 2
+        if nSpecies == 1
+            vSpace = VSpace2D(umin, umax, nu, vmin, vmax, nv, vMeshType, nug, nvg)
+        elseif nSpecies == 2
+            ue0 = umin * sqrt(mi / me)
+            ue1 = umax * sqrt(mi / me)
+            ve0 = vmin * sqrt(mi / me)
+            ve1 = vmax * sqrt(mi / me)
+            vSpace = MVSpace2D(
+                umin,
+                umax,
+                ue0,
+                ue1,
+                nu,
+                vmin,
+                vmax,
+                ve0,
+                ve1,
+                nv,
+                vMeshType,
+                nug,
+                nvg,
+            )
+        else
+            throw("The velocity space only supports up to two species.")
+        end
+    elseif Dv == 3
+        if nSpecies == 1
+            vSpace = VSpace3D(
+                umin,
+                umax,
+                nu,
+                vmin,
+                vmax,
+                nv,
+                wmin,
+                wmax,
+                nw,
+                vMeshType,
+                nug,
+                nvg,
+                nwg,
+            )
+        elseif nSpecies == 2
+            ue0 = umin * sqrt(mi / me)
+            ue1 = umax * sqrt(mi / me)
+            ve0 = vmin * sqrt(mi / me)
+            ve1 = vmax * sqrt(mi / me)
+            we0 = wmin * sqrt(mi / me)
+            we1 = wmax * sqrt(mi / me)
+            vSpace = MVSpace3D(
+                umin,
+                umax,
+                ue0,
+                ue1,
+                nu,
+                vmin,
+                vmax,
+                ve0,
+                ve1,
+                nv,
+                wmin,
+                wmax,
+                we0,
+                we1,
+                nw,
+                vMeshType,
+                nug,
+                nvg,
+                nwg,
+            )
+        else
+            throw("The velocity space only supports up to two species.")
+        end
+    end
+
+    return vSpace
+end
+
+
+"""
+Generate AbstractProperty
+
+"""
+function set_property(dict::T) where {T<:AbstractDict}
+    for key in keys(dict)
+        s = Symbol(key)
+        @eval $s = $(dict[key])
+    end
+
+    Dx = parse(Int, space[1])
+    γD = map(parse(Int, space[3]), parse(Int, space[5])) do x, y # (x)f(y)v
+        if x == 1 # 1f
+            if y >= 3 # 3v
+                return 3
+            else # 1v
+                return Dx
             end
+        elseif x == 2 # 2f
+            return Dx
+        elseif x >= 3 # 3f / 4f
+            return 3
         else
-            throw("The gas property only supports up to two species.")
+            return nothing
         end
+    end
+    γ = heat_capacity_ratio(inK, γD)
 
-        if @isdefined uLid
-            ib = set_ib(set, vSpace, gas, uLid, vLid, tLid)
+    if nSpecies == 1
+        μᵣ = ref_vhs_vis(knudsen, alphaRef, omegaRef)
+        gas = Gas(knudsen, mach, prandtl, inK, γ, omega, alphaRef, omegaRef, μᵣ)
+    elseif nSpecies == 2
+        kne = knudsen * (me / mi)
+        if !(@isdefined rL) && !(@isdefined echi) # undefined plasma args
+            gas = Mixture([knudsen, kne], mach, prandtl, inK, γ, mi, ni, me, ne)
         else
-            ib = set_ib(set, vSpace, gas)
+            if Dx == 1
+                gas = Plasma1D(
+                    [knudsen, kne],
+                    mach,
+                    prandtl,
+                    inK,
+                    γ,
+                    mi,
+                    ni,
+                    me,
+                    ne,
+                    lD,
+                    rL,
+                    sol,
+                    echi,
+                    bnu,
+                )
+            elseif Dx == 2
+                gas = Plasma2D(
+                    [knudsen, kne],
+                    mach,
+                    prandtl,
+                    inK,
+                    γ,
+                    mi,
+                    ni,
+                    me,
+                    ne,
+                    lD,
+                    rL,
+                    sol,
+                    echi,
+                    bnu,
+                )
+            else
+                throw("The plasma property only supports up to 2D case.")
+            end
         end
+    else
+        throw("The gas property only supports up to two species.")
+    end
 
-        # create working directory
-        identifier = string(Dates.now(), "/")
-        #outputFolder = string("../out/", replace(identifier, ":"=>"."))
-        outputFolder = replace(identifier, ":" => ".")
-        mkdir(outputFolder)
-        mkdir(string(outputFolder, "data/"))
-        cp(configfilename, string(outputFolder, "config.txt"))
-
-        # create new struct
-        new(set, pSpace, vSpace, gas, ib, outputFolder)
-
-    end # function
-
-end # struct
+    return gas
+end
 
 
 """
