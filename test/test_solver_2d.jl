377f7c8239c5f1d370d3b7a1c5fcbf5e62f7d150
Tianbai Xiao <tianbaixiao@gmail.com>
2021-01-13 17:27:21 +0100

    Add solve! for 2d2f case

--- test/test_solver_2d.jl
+++ test/test_solver_2d.jl
@@ -3,6 +3,11 @@ import KitBase
 cd(@__DIR__)
 ks, ctr, a1face, a2face, simTime = KitBase.initialize("config_2d2f.txt")
 
+res = zeros(4)
 dt = KitBase.timestep(ks, ctr, simTime)
-KitBase.reconstruct!(ks, ctr)
-KitBase.evolve!(ks, ctr, a1face, a2face, dt; mode = :kfvs, bc = :maxwell)
+
+for iter = 1:2
+    KitBase.reconstruct!(ks, ctr)
+    KitBase.evolve!(ks, ctr, a1face, a2face, dt; mode = :kfvs, bc = :maxwell)
+    KitBase.update!(ks, ctr, a1face, a2face, dt, res; coll = :bgk, bc = :maxwell)
+end
