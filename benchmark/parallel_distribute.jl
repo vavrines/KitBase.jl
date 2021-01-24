6fd6bd469b361400556c17bf6770d24782441642
Tianbai Xiao <tianbaixiao@gmail.com>
2021-01-24 12:05:11 +0100

    Finish distributed parallel example

--- benchmark/parallel_distribute.jl
+++ benchmark/parallel_distribute.jl
@@ -1,8 +1,6 @@
-using Distributed, SharedArrays, BenchmarkTools
-addprocs(1)
+@everywhere using Distributed, SharedArrays
+addprocs(3)
 @everywhere using KitBase
-
-#import KitBase
 
 begin
     vars = Dict{String,Any}()
@@ -18,7 +16,7 @@ begin
     vars["maxTime"] = 0.2
     vars["x0"] = 0.0
     vars["x1"] = 1.0
-    vars["nx"] = 1000
+    vars["nx"] = 2000
     vars["pMeshType"] = "uniform"
     vars["nxg"] = 0
     vars["knudsen"] = 0.001
@@ -38,59 +36,8 @@ ib = KitBase.set_ib(vars, set, vSpace, gas)
 folder = @__DIR__
 ks = KitBase.SolverSet(set, pSpace, vSpace, gas, ib, folder)
 
-dt = ks.pSpace.dx[1] / (1.0 + KitBase.sound_speed(ks.ib.primL, ks.gas.γ))
+dt = ks.pSpace.dx[1] / (5.0 + KitBase.sound_speed(ks.ib.primL, ks.gas.γ))
 nt = floor(ks.set.maxTime / dt) |> Int
-
-#--- serial ---#
-w = zeros(ks.pSpace.nx, 3)
-for i in 1:ks.pSpace.nx
-    if i <= ks.pSpace.nx ÷ 2
-        w[i,:] .= ks.ib.wL
-    else
-        w[i,:] .= ks.ib.wR
-    end
-end     
-fw = zeros(ks.pSpace.nx+1, 3)
-
-i = 2
-flux = @view fw[i,:]
-KitBase.flux_gks!(
-    flux,
-    w[i-1,:],
-    w[i,:],
-    ks.gas.γ,
-    ks.gas.K,
-    ks.gas.μᵣ,
-    ks.gas.ω,
-    dt,
-    0.5 * ks.pSpace.dx[i-1],
-    0.5 * ks.pSpace.dx[i],
-)
-
-
-@time for iter = 1:nt
-    for i in 2:ks.pSpace.nx
-        flux = @view fw[i,:]
-        flux_gks!(
-            flux,
-            w[i-1,:],
-            w[i,:],
-            ks.gas.γ,
-            ks.gas.K,
-            ks.gas.μᵣ,
-            ks.gas.ω,
-            dt,
-            0.5 * ks.pSpace.dx[i-1],
-            0.5 * ks.pSpace.dx[i],
-        )
-    end
-    
-    for i in 2:ks.pSpace.nx-1
-        for j in 1:3
-            w[i,j] += (fw[i,j] - fw[i+1,j]) / ks.pSpace.dx[i]
-        end
-    end
-end
 
 #--- parallel ---#
 wp = SharedArray{Float64}((ks.pSpace.nx, 3), init=A->(A=zeros(ks.pSpace.nx, 3)))
@@ -103,10 +50,10 @@ for i in 1:ks.pSpace.nx
 end     
 fwp = SharedArray{Float64}((ks.pSpace.nx+1, 3), init=A->(A=zeros(ks.pSpace.nx+1, 3)))
 
-@time for iter = 1:nt
+@time for iter = 1:nt÷3
     @sync @distributed for i in 2:ks.pSpace.nx
         flux = @view fwp[i,:]
-        flux_gks!(
+        KitBase.flux_gks!(
             flux,
             wp[i-1,:],
             wp[i,:],
@@ -126,3 +73,40 @@ fwp = SharedArray{Float64}((ks.pSpace.nx+1, 3), init=A->(A=zeros(ks.pSpace.nx+1,
         end
     end
 end
+"""~13.620491 seconds (2.26 M allocations: 101.219 MiB, 0.22% gc time)"""
+
+#--- serial ---#
+w = zeros(ks.pSpace.nx, 3)
+for i in 1:ks.pSpace.nx
+    if i <= ks.pSpace.nx ÷ 2
+        w[i,:] .= ks.ib.wL
+    else
+        w[i,:] .= ks.ib.wR
+    end
+end     
+fw = zeros(ks.pSpace.nx+1, 3)
+
+@time for iter = 1:nt÷3
+    for i in 2:ks.pSpace.nx
+        flux = @view fw[i,:]
+        KitBase.flux_gks!(
+            flux,
+            w[i-1,:],
+            w[i,:],
+            ks.gas.γ,
+            ks.gas.K,
+            ks.gas.μᵣ,
+            ks.gas.ω,
+            dt,
+            0.5 * ks.pSpace.dx[i-1],
+            0.5 * ks.pSpace.dx[i],
+        )
+    end
+    
+    for i in 2:ks.pSpace.nx-1
+        for j in 1:3
+            w[i,j] += (fw[i,j] - fw[i+1,j]) / ks.pSpace.dx[i]
+        end
+    end
+end
+"""~20.830331 seconds (323.96 M allocations: 24.472 GiB, 16.89% gc time)"""
