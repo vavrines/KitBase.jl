4c7e50e7764131d1e8773819d2f23d7d11c3b9d9
vavrines <tianbaixiao@gmail.com>
2021-04-11 23:59:11 +0200

    Update build

--- deps/build.jl
+++ deps/build.jl
@@ -7,16 +7,17 @@ import Pkg
 # 2) global pip installer
 
 #ENV["PYTHON"] = "/usr/bin/python3"
-ENV["PYTHON"] = "~/.julia/conda/3/bin/python"
-Pkg.build("PyCall")
+#ENV["PYTHON"] = "~/.julia/conda/3/bin/python"
+#Pkg.build("PyCall")
 
-Conda.add_channel("conda-forge")
-Conda.add("meshio")
+#Conda.add_channel("conda-forge")
+#Conda.add("meshio")
 #Conda.pip_interop(true)
 #Conda.pip("install", "meshio")
 
-#@info "installing meshio"
+@info "installing meshio"
 #cmd = `pip3 install meshio --user`
-#run(cmd)
+cmd = `./usr/bin/pip3 install meshio --user`
+run(cmd)
 
 meshio = pyimport("meshio")
