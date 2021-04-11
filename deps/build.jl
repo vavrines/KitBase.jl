c1c187a82f23d8953cd64e5857d19d1d2d97f18f
vavrines <tianbaixiao@gmail.com>
2021-04-12 00:28:22 +0200

    Sync

--- deps/build.jl
+++ deps/build.jl
@@ -1,28 +1,16 @@
-using PyCall, Conda
-import Pkg
+using PyCall
 
 # try importing meshio
 # catch the installation
 # 1) Julia built-in miniconda
 # 2) global pip installer
-
-JULIA_KIT_DIR = homedir()
-
-#ENV["PYTHON"] = "/usr/bin/python"
-ENV["PYTHON"] = "$(JULIA_KIT_DIR)/.julia/conda/3/bin/python"
-Pkg.build("PyCall")
-
-PIP = "$(JULIA_KIT_DIR)/.julia/conda/3/bin/pip"
-run(`$PIP install meshio --user`)
-
-#Conda.add_channel("conda-forge")
-#Conda.add("meshio")
-#Conda.pip_interop(true)
-#Conda.pip("install", "meshio")
-
-#@info "installing meshio"
-#cmd = `pip install meshio --user`
-#cmd = `./usr/bin/pip install meshio --user`
-#run(cmd)
-
-meshio = pyimport("meshio")
+try
+    using Conda
+    Conda.add_channel("conda-forge")
+    Conda.add("meshio")
+    meshio = pyimport("meshio")
+catch
+    cmd = `pip3 install meshio --user`
+    run(cmd)
+    meshio = pyimport("meshio")
+end
