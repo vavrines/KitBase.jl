15ee643f85a8a774afaf7324ee71dedc8680f166
vavrines <tianbaixiao@gmail.com>
2021-04-11 23:25:20 +0200

    CI -> 1.5

--- deps/build.jl
+++ deps/build.jl
@@ -1,12 +1,12 @@
-using PyCall#, Conda
+using PyCall, Conda
 
 # try importing meshio
 # catch the installation
 # 1) Julia built-in miniconda
 # 2) global pip installer
 
-#Conda.add_channel("conda-forge")
-#Conda.add("meshio")
+Conda.add_channel("conda-forge")
+Conda.add("meshio")
 #Conda.pip_interop(true)
 #Conda.pip("install", "meshio")
 
