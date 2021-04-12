3fcf0af5d4d018987ca48d7796f01f34a884a3c8
vavrines <tianbaixiao@gmail.com>
2021-04-12 10:55:51 +0200

    Fix test

--- deps/build.jl
+++ deps/build.jl
@@ -5,13 +5,13 @@ using PyCall, Conda
 # 1) Julia built-in miniconda
 # 2) global pip installer
 
-Conda.add_channel("conda-forge")
-Conda.add("meshio")
-#Conda.pip_interop(true)
-#Conda.pip("install", "meshio")
-
-@info "installing meshio"
-cmd = `pip3 install meshio --user`
-run(cmd)
-
-meshio = pyimport("meshio")
+try
+    using Conda
+    Conda.add_channel("conda-forge")
+    Conda.add("meshio")
+    meshio = pyimport("meshio")
+catch
+    cmd = `pip3 install meshio --user`
+    run(cmd)
+    meshio = pyimport("meshio")
+end
