9530ea7db769a07acb60e518250fe140e5167b0e
vavrines <tianbaixiao@gmail.com>
2021-05-21 12:42:55 +0200

    Remove constructor for Interface1D & 1D1F

--- src/Type/struct_face.jl
+++ src/Type/struct_face.jl
@@ -8,25 +8,15 @@
 # ------------------------------------------------------------
 
 """
-    mutable struct Interface1D{A} <: AbstractInterface1D
+    mutable struct Interface1D{A<:Union{Real,AbstractArray}} <: AbstractInterface1D
         fw::A
-
-        function Interface1D(w::Union{Real,AbstractArray})
-            fw = zero(w)
-            new{typeof(fw)}(fw)
-        end
     end
 
 1D cell interface with no distribution function
 
 """
-mutable struct Interface1D{A} <: AbstractInterface1D
+mutable struct Interface1D{A<:Union{Real,AbstractArray}} <: AbstractInterface1D
     fw::A
-
-    function Interface1D(w::Union{Real,AbstractArray})
-        fw = zero(w)
-        new{typeof(fw)}(fw)
-    end
 end
 
 function Base.show(io::IO, ctr::Interface1D{A}) where {A}
@@ -50,16 +40,9 @@ end
 1D cell interface with 1 distribution function
 
 """
-mutable struct Interface1D1F{A,B} <: AbstractInterface1D
+mutable struct Interface1D1F{A,B<:AbstractArray} <: AbstractInterface1D
     fw::A
     ff::B
-
-    function Interface1D1F(w::AbstractArray, f::AbstractArray)
-        fw = zero(w)
-        ff = zero(f)
-
-        new{typeof(fw),typeof(ff)}(fw, ff)
-    end
 end
 
 function Base.show(io::IO, ctr::Interface1D1F{A,B}) where {A,B}
@@ -90,13 +73,13 @@ end
 1D cell interface with 2 distribution functions
 
 """
-mutable struct Interface1D2F{A,B} <: AbstractInterface1D
+mutable struct Interface1D2F{A,B<:AbstractArray} <: AbstractInterface1D
     fw::A
     fh::B
     fb::B
 end
 
-function Interface1D2F(w::AbstractArray, f::AbstractArray)
+function Interface1D2F(w, f::AbstractArray)
     fw = zero(w)
     fh = zero(f)
     fb = zero(f)
