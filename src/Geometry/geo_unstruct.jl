5ebf55ee2fb4551602c8985f958ad2d57caed940
vavrines <tianbaixiao@gmail.com>
2021-06-22 10:18:33 +0200

    Change unstructured solverset api with FiniteMesh

--- src/Geometry/geo_unstruct.jl
+++ src/Geometry/geo_unstruct.jl
@@ -16,56 +16,45 @@
 
 Physical space with unstructured mesh
 """
-struct UnstructPSpace{A,B,C,D,E,F,G,H,I,J,K,L} <: AbstractPhysicalSpace
+struct UnstructPSpace{
+    A,
+    B<:AbstractMatrix{<:AbstractFloat},
+    C<:AbstractMatrix{<:Integer},
+    D<:AbstractVector{<:Integer},
+    E<:AbstractVector{<:AbstractFloat},
+    F<:AbstractArray{<:AbstractFloat,3},
+} <: AbstractPhysicalSpace
     cells::A # all information: cell, line, vertex
     points::B # locations of vertex points
     cellid::C # node indices of elements
     cellType::D # inner/boundary cell
-    cellNeighbors::E # neighboring cells id
-    cellFaces::F # cell edges id
-    cellCenter::G # cell center location
-    cellArea::H # cell size
-    facePoints::I # ids of two points at edge
-    faceCells::J # ids of two cells around edge
-    faceCenter::K # edge center location
-    faceType::L
+    cellNeighbors::C # neighboring cells id
+    cellFaces::C # cell edges id
+    cellCenter::B # cell center location
+    cellArea::E # cell size
+    cellNormals::F # cell unit normal vectors
+    facePoints::C # ids of two points at edge
+    faceCells::C # ids of two cells around edge
+    faceCenter::B # edge center location
+    faceType::D # inner/boundary face
+    faceArea::E # face area
 end
 
 function UnstructPSpace(file::T) where {T<:AbstractString}
     cells, points = read_mesh(file)
-    cellid = extract_cell(cells)
-    edgePoints, edgeCells, cellNeighbors = mesh_connectivity_2D(cellid)
-    cellType = mesh_cell_type(cellNeighbors)
-    cellArea = mesh_area_2D(points, cellid)
-    cellCenter = mesh_center_2D(points, cellid)
-    edgeCenter = mesh_face_center(points, edgePoints)
-    cellEdges = mesh_cell_face(cellid, edgeCells)
-    edgeType = mesh_face_type(edgeCells, cellType)
+    p = mesh_connectivity_2D(cells, points)
 
-    return UnstructPSpace(
-        cells,
-        points,
-        cellid,
-        cellType,
-        cellNeighbors,
-        cellEdges,
-        cellCenter,
-        cellArea,
-        edgePoints,
-        edgeCells,
-        edgeCenter,
-        edgeType,
-    )
+    return UnstructPSpace((cells, points)..., p...)
 end
 
 
 function write_vtk(ks::T1, ctr) where {T1<:AbstractSolverSet}
-    KitBase.write_vtk(ks.ps, ctr)
+    write_vtk(ks.ps, ctr)
 
     return nothing
 end
 
-function KitBase.write_vtk(ps::UnstructPSpace, ctr)
+function write_vtk(ps::UnstructPSpace, ctr)
     cdata = zeros(length(ctr), length(ctr[1].w))
     for i in eachindex(ctr)
         cdata[i, :] .= ctr[i].prim
@@ -73,7 +62,7 @@ function KitBase.write_vtk(ps::UnstructPSpace, ctr)
             cdata[i, end] = 1.0 / cdata[i, end]
         end
     end
-    KitBase.write_vtk(ps.points, ps.cellid, cdata)
+    write_vtk(ps.points, ps.cellid, cdata)
 
     return nothing
 end
