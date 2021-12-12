f07f36bbf7be6877c464d92f00496b68c19e4176
vavrines <tianbaixiao@gmail.com>
2021-12-12 19:52:31 +0100

    Add tecplot writer

--- test/test_io.jl
+++ test/test_io.jl
@@ -10,3 +10,14 @@ KitBase.write_jld(ks, ctr)
 
 KitBase.plot_line(ks, ctr)
 plot(ks, ctr, legend = :none)
+
+# tecplot writer
+x = zeros(5, 3)
+y = zero(x)
+for i = 1:5, j = 1:3
+    x[i, j] = 0.25 * (i-1)
+    y[i, j] = 0.5 * (j-1)
+end
+sol = randn(4, 2, 2)
+
+write_tec(x, y, sol)
