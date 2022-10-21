2ed0c2b891f25c4ea2a0b4c1d8c9949ee615754c
vavrines <tianbaixiao@gmail.com>
2022-10-21 16:10:26 +0200

    Add extract_sol method

--- src/IO/io.jl
+++ src/IO/io.jl
@@ -2,9 +2,11 @@
 # I / O Methods
 # ============================================================
 
+export extract_sol
 export read_cfg, read_dict
 export write_sol, write_jld, write_bson, write_vtk, write_tec
 
+include("io_process.jl")
 include("io_read.jl")
 include("io_write.jl")
 include("io_plot.jl")
