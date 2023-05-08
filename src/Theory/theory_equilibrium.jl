6895a696e18552110620ee571a4265a32c1036e8
vavrines <tianbaixiao@gmail.com>
2023-05-08 15:24:48 +0800

    Fix typo

--- src/Theory/theory_equilibrium.jl
+++ src/Theory/theory_equilibrium.jl
@@ -844,7 +844,7 @@ end
 """
 $(SIGNATURES)
 
-Calculate dimensionless rotationa number in Rykov model
+Calculate dimensionless rotation number in Rykov model
 
 """
 rykov_zr(Tₜ, T₀, Z₀) = Z₀ / (1.0 + π^1.5 / 2.0 * sqrt(T₀ / Tₜ) + (π + 0.25 * π^2) * T₀ / Tₜ)
