de7550c422dd8d806cfb89920a616e7ed9c4843e
vavrines <tianbaixiao@gmail.com>
2024-01-15 14:54:24 +0800

    Add wall solution extractor

--- src/IO/io_process.jl
+++ src/IO/io_process.jl
@@ -37,3 +37,51 @@ function extract_sol(ps::AbstractPhysicalSpace2D, ctr)
 
     return sol
 end
+
+
+"""
+$(SIGNATURES)
+
+Extract wall quantities
+"""
+function extract_wall(bc, h, b, u, v, ω, inK, γ, rot)
+    δ = heaviside.(u .* rot)
+
+    SF = sum(ω .* u .* h .* (1.0 .- δ))
+    SG =
+        (bc[end] / π) *
+        sum(ω .* u .* exp.(-bc[end] .* ((u .- bc[2]) .^ 2 .+ (v .- bc[3]) .^ 2)) .* δ)
+    prim = [-SF / SG; bc[2:end]]
+
+    H = maxwellian(u, v, prim)
+    B = H .* inK ./ (2.0 * prim[end])
+
+    hWall = H .* δ .+ h .* (1.0 .- δ)
+    bWall = B .* δ .+ b .* (1.0 .- δ)
+
+    w = moments_conserve(hWall, bWall, u, v, ω, VDF{2,2})
+    prim = conserve_prim(w, γ)    
+
+    wp = pressure(hWall, bWall, prim, u, v, ω, inK, VDF{2,2})
+    ws = sum(ω .* u * v * hWall)
+    wq = heat_flux(hWall, bWall, prim, u, v, ω) |> norm
+
+    return wp, ws, wq
+end
+
+function extract_wall(ctr, bc, vs, inK, γ, n, rot)
+    vn, vt = local_velocity(vs.u, vs.v, n)
+    bcL = local_frame(bc, n)
+
+    return extract_wall(
+        bcL,
+        ctr.h,
+        ctr.b,
+        vn,
+        vt,
+        vs.weights,
+        inK,
+        γ,
+        rot,
+    )
+end
--- test/test_solver_2d.jl
+++ test/test_solver_2d.jl
@@ -14,6 +14,8 @@ KitBase.update!(ks, ctr, a1face, a2face, dt, zeros(4); coll = :bgk, bc = :mirror
 c1 = deepcopy(ctr[1])
 KB.bc_riemann!(c1, ctr[1], ks.vs.u, ks.vs.v, [1.0, 0.5, 0.0, 1.0], 5 / 3, 2, [1.0 / √2, 1.0 / √2])
 
+KB.extract_wall(c1, [1.0, 0.5, 0.0, 1.0], ks.vs, 1, 5/3, [1.0 / √2, 1.0 / √2], 1)
+
 ks, ctr, a1face, a2face, simTime = KitBase.initialize("config_2d1f.txt")
 KitBase.reconstruct!(ks, ctr)
 KitBase.update!(ks, ctr, a1face, a2face, dt, zeros(4); coll = :bgk, bc = :maxwell)
