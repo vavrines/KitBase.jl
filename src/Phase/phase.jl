8655a1d1f57b2bfa52417cfa7a908ad44d83ed71
vavrines <tianbaixiao@gmail.com>
2024-07-05 14:25:23 +0800

    Add mesh_quadrature

--- src/Phase/phase.jl
+++ src/Phase/phase.jl
@@ -4,10 +4,9 @@
 
 export VSpace1D, VSpace2D, VSpace3D, UnstructVSpace
 export MVSpace1D, MVSpace2D, MVSpace3D
-export set_velocity
-export maxwell_quadrature
-export legendre_quadrature, octa_quadrature
+export set_velocity, mesh_quadrature
 
-include("creamer.jl")
-include("quadrature.jl")
-include("velocity.jl")
+include("phase_creamer.jl")
+include("phase_quadrature.jl")
+include("phase_mesh.jl")
+include("phase_velocity.jl")
