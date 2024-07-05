5de6237073b64e0ed3a3aad6cd296b3e1b947be7
vavrines <tianbaixiao@gmail.com>
2024-07-05 17:20:15 +0800

    Update geometry test

--- test/test_geo.jl
+++ test/test_geo.jl
@@ -1,29 +1,35 @@
 import KitBase.AA
 
+# 2D
 w = [1.0, 0.1, 0.3, 1.0]
 cosa = cos(0.5)
 sina = sin(0.5)
-KitBase.global_frame(randn(2), cosa, sina)
-KitBase.local_frame(randn(2), cosa, sina)
-KitBase.global_frame(w, cosa, sina)
-KitBase.local_frame(w, cosa, sina)
+KB.global_frame(randn(2), cosa, sina)
+KB.global_frame(randn(2), [cosa, sina])
+KB.global_frame([1, 0], cosa, sina)
+KB.local_frame(randn(2), cosa, sina)
+KB.local_frame(randn(2), [cosa, sina])
+KB.local_frame([1, 0], cosa, sina)
+KB.global_frame(w, cosa, sina)
+KB.local_frame(w, cosa, sina)
 
-KitBase.global_frame(rand(3), rand(3, 3))
-KitBase.local_frame(rand(3), rand(3, 3))
-KitBase.global_frame(rand(5), rand(3, 3))
-KitBase.local_frame(rand(5), rand(3, 3))
+# 3D
+KB.global_frame(rand(3), rand(3, 3))
+KB.local_frame(rand(3), rand(3, 3))
+KB.global_frame(rand(5), rand(3, 3))
+KB.local_frame(rand(5), rand(3, 3))
 
-KitBase.unit_normal(rand(2), rand(2))
-KitBase.unit_normal(rand(3), rand(3), rand(3))
-KitBase.point_distance(rand(2), rand(2), rand(2))
+KB.unit_normal(rand(2), rand(2))
+KB.unit_normal(rand(3), rand(3), rand(3))
+KB.point_distance(rand(2), rand(2), rand(2))
 
 #--- structure mesh ---#
-KitBase.PSpace1D()
-KitBase.PSpace1D(0.0, 1.0) |> show
-KitBase.PSpace2D()
-KitBase.PSpace2D(0.0, 1.0, 0.0, 1.0) |> show
-KitBase.CSpace2D(0.0, 1.0, 10, 0.0, π, 10) |> show
-KitBase.PSpace3D{Int,Int,AA{Float64,3},AA{Float64,5},AA{Float64,4},AA{Float64,4}}(
+KB.PSpace1D()
+KB.PSpace1D(0.0, 1.0) |> show
+KB.PSpace2D()
+KB.PSpace2D(0.0, 1.0, 0.0, 1.0) |> show
+KB.CSpace2D(0.0, 1.0, 10, 0.0, π, 10) |> show
+KB.PSpace3D{Int,Int,AA{Float64,3},AA{Float64,5},AA{Float64,4},AA{Float64,4}}(
     0,
     1,
     10,
@@ -47,25 +53,25 @@ KitBase.PSpace3D{Int,Int,AA{Float64,3},AA{Float64,5},AA{Float64,4},AA{Float64,4}
 x0 = 0.0
 nx = 15
 dx = 0.1
-KitBase.uniform_mesh(x0, nx, dx)
+KB.uniform_mesh(x0, nx, dx)
 
 x = collect(0:0.1:1)
 y = collect(0:0.1:1)
 z = collect(0:0.1:1)
-KitBase.ndgrid(x)
-KitBase.ndgrid(x, y)
-KitBase.ndgrid(x, y, z)
-KitBase.meshgrid(x)
-KitBase.meshgrid(x, y)
-KitBase.meshgrid(x, y, z)
+KB.ndgrid(x)
+KB.ndgrid(x, y)
+KB.ndgrid(x, y, z)
+KB.meshgrid(x)
+KB.meshgrid(x, y)
+KB.meshgrid(x, y, z)
 
-KitBase.find_idx(randn(20), 0.13, mode = :uniform)
-KitBase.find_idx(randn(20), 0.13, mode = :nonuniform)
+KB.find_idx(randn(20), 0.13, mode = :uniform)
+KB.find_idx(randn(20), 0.13, mode = :nonuniform)
 
 #--- unstructure mesh ---#
 cd(@__DIR__)
-ps = KitBase.UnstructPSpace("t1.msh")
-KitBase.write_vtk(ps.points, ps.cellid, randn(size(ps.cellid, 1)))
+ps = KB.UnstructPSpace("t1.msh")
+KB.write_vtk(ps.points, ps.cellid, randn(size(ps.cellid, 1)))
 
 #--- IB ---#
 ps = KB.PSpace2D(0, 3, 15, 0, 2, 10, 1, 1)
@@ -97,5 +103,5 @@ for iter in axes(xbis, 1)
 end
 xips = KB.ip_location(ps, ghost_ids, xbis)
 ip_cids, ip_nids, ip_bids = KB.ip_connectivity(ps, xips, flags)
-ib = SharpIB(flags, ghost_ids, xbis, nbis, xips, ip_cids, ip_nids, ip_bids)
+ib = KB.SharpIB(flags, ghost_ids, xbis, nbis, xips, ip_cids, ip_nids, ip_bids)
 KB.bilinear_coeffs(ps, ib, 1, rand(2))
